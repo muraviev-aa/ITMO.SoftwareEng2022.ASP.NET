b0727625080562007393f0a953236bd3e822e640
muraviev-aa <muraviev-aa@yandex.ru>
2022-10-06 07:48:12 +0300

    06.10.2022

---
+++ Lab7/ITMO.ASP.NET.Course2022/ITMO.ASP.NET.Course2022.Lab/Site1.Master.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ITMO.ASP.NET.Course2022.Lab
+{
+    public partial class Site1 : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            long numVisitors = 0;
+            if (Application["Visitors"] != null) 
+            { 
+                numVisitors = long.Parse(Application["Visitors"].ToString());
+                VisitorLiteral.Text = "Число посещений: " + numVisitors.ToString();
+            }
+        }
+
+        protected void NavigationMenu_MenuItemClick(object sender, MenuEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Lab7/ITMO.ASP.NET.Course2022/ITMO.ASP.NET.Course2022.Lab/Start.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Start.aspx.cs" Inherits="ITMO.ASP.NET.Course2022.Lab.Start" MasterPageFile="~/Site1.Master" %>
+
+<asp:Content ID="MainContent" ContentPlaceHolderID="ContentPlaceHolder1" runat="server"> 
+     <div>
+        <div class="rek"> 
+            <h1>Приглашение на семинар</h1> 
+            <p>Вы приглашены на наш семинар</p> 
+            <p>Подтвердите свое согласие, пройдя регистрацию</p> 
+
+        </div> 
+            <div class="info"> Семинар состоится 1 января 2020 года в 7.30 </div>
+        </div>
+</asp:Content>
+       
