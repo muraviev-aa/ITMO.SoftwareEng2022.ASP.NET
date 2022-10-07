5f73b6c7cb9b7621daf98af55b43687939edadad
muraviev-aa <muraviev-aa@yandex.ru>
2022-10-07 09:49:03 +0300

    07.10.2022

---
+++ ASP.NET_MVC/Lab2/ITMO.ASP.MVC.Course2022/ITMO.ASP.MVC.Course2022.Lab/Models/ModelClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITMO.ASP.MVC.Course2022.Lab.Models
+{
+    public class ModelClass
+    {
+        public static string ModelHello()
+        {
+            int hour = DateTime.Now.Hour;
+            string Greeting = hour < 12 ? "Доброе утро" : "Добрый день"; return Greeting;
+        }
+    }
+}
