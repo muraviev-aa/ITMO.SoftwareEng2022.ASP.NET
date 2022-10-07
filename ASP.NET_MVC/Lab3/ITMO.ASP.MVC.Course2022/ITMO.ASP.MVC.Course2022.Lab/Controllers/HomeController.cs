97b1b98e96be36eea327100b7d5c270f458a7e07
muraviev-aa <muraviev-aa@yandex.ru>
2022-10-07 14:35:31 +0300

    07.10.2022

---
+++ ASP.NET_MVC/Lab3/ITMO.ASP.MVC.Course2022/ITMO.ASP.MVC.Course2022.Lab/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using ITMO.ASP.MVC.Course2022.Lab.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using WebMVCR1.Models;
+
+namespace ITMO.ASP.MVC.Course2022.Lab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static PersonRepository db = new PersonRepository();
+        //GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Добрый день";
+            ViewData["Mes"] = "хорошего настроения";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult InputData() 
+        { 
+            return View(); 
+        }
+
+        [HttpPost]
+        public ViewResult InputData(Person p)
+        {
+            db.AddResponse(p);
+            return View("Hello", p);
+        }
+
+        public ViewResult OutputData() 
+        { 
+            ViewBag.Pers = db.GetAllResponses;
+            ViewBag.Count = db.NumberOfPerson;
+            return View("ListPerson"); 
+        }
+
+        //public string Index(string hel)
+        //{
+        //    string Greeting = ModelClass.ModelHello() + ", " + hel;
+        //    return Greeting;
+        //}
+    }
+}
---
+++ ASP.NET_MVC/Lab3/ITMO.ASP.MVC.Course2022/ITMO.ASP.MVC.Course2022.Lab/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITMO.ASP.MVC.Course2022.Lab.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public override string ToString() { string s = FirstName + " " + LastName; return s; }
+    }
+}
